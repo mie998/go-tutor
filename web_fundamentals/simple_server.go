f8dc00bfc3e6a504ab78177739adbfc5cf6d8d89
mie998 <nishiwaki.kyoto@gmail.com>
2021-10-04 22:06:35 +0900

    web-fundamentals'

--- web_fundamentals/simple_server.go
+++ web_fundamentals/simple_server.go
@@ -16,8 +16,18 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello astaxie!")
 }
 
+func handleRouter(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello router!")
+}
+
 func main() {
 	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/*", handleRouter)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServer: ", err)
